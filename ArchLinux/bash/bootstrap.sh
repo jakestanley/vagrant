31b0b7bb7470ad6e6893dcb84dac9bfd0e70736c
Jake Stanley <jacob.stanley@hermes-europe.co.uk>
2018-11-08 10:09:57 +0000

    get sublime repository gpg key in bootstrap.sh

--- ArchLinux/bash/bootstrap.sh
+++ ArchLinux/bash/bootstrap.sh
@@ -11,3 +11,9 @@ rm /etc/localtime
 ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
 
 mkdir -p /var/lib/docker
+
+# install sublime GPG keys
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg
+pacman-key --add sublimehq-pub.gpg
+pacman-key --lsign-key 8A8F901A
+rm sublimehq-pub.gpg
